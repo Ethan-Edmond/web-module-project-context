c169d249f085ac7e296e115b9e1e080f293054ce
Ethan-Edmond <eedmond@uccs.edu>
2021-06-10 13:38:32 -0600

    Added CartContext to ShoppingCart

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
 			    </Route>
 
 			    <Route path="/cart">
-				    <ShoppingCart cart={cart} />
+				    <ShoppingCart/>
 			    </Route>
         </CartContext.Provider>
       </ProductContext.Provider>
